14de39d41254b516585f629c0b28d92c9d2d5d69
amorenew <amorenew@gmail.com>
2014-12-05 15:56:01 +0200

    add open lecture button

--- ILS/src/com/orangelabs/rcs/ri/activities/DialActivity.java
+++ ILS/src/com/orangelabs/rcs/ri/activities/DialActivity.java
@@ -5,23 +5,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 
 import com.orangelabs.rcs.ri.R;
 
-public class DialActivity extends Activity implements OnClickListener {
+public class DialActivity extends Activity {
+	Button btnLecture;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_dial);
-		
-	}
-
-	@Override
-	public void onClick(View v) {
-        Intent lectureIntent = new Intent(DialActivity.this,LectureActivity.class);
-        startActivity(lectureIntent);
-        finish();
+		btnLecture = (Button) findViewById(R.id.btnLecture);
+		btnLecture.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent lectureIntent = new Intent(DialActivity.this,
+						LectureActivity.class);
+				startActivity(lectureIntent);
+				finish();
+			}
+		});
 	}
 
 }
