62a23b5c6ccae7edec9c2cb5eec775263faf706c
amorenew <amorenew@gmail.com>
2014-12-05 14:20:37 +0200

    add splash and dial activites

---
+++ ILS/src/com/orangelabs/rcs/ri/activities/DialActivity.java
@@ -0,0 +1,19 @@
+package com.orangelabs.rcs.ri.activities;
+
+import com.orangelabs.rcs.ri.R;
+import com.orangelabs.rcs.ri.R.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class DialActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_dial);
+	}
+
+}
